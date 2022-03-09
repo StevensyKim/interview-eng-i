e651456b19e650f7112f99531b9e26b7f1a58ed5
steven kim <steven.sy.kim@gmail.com>
2022-03-08 21:02:39 -0500

    task6 changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,7 @@ import ImageCarousel from './ImageCarousel';
 export default function Home({imageData}) {
   if (!imageData || imageData.length === 0) {
     return (
-      // TODO: create loader image
-      <p>No ImageData Yet</p>
+      <img src='spinner.gif' alt='loading...' />
     );
   }
   return (
