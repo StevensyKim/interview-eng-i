7f1656007ce31976f46db08d3afdb28a29dccf7c
steven kim <steven.sy.kim@gmail.com>
2022-03-09 14:10:11 -0500

    loader

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,7 @@ import ImageCarousel from './ImageCarousel';
 export default function Home({imageData}) {
   if (!imageData || imageData.length === 0) {
     return (
-      <img src='spinner.gif' alt='loading...' style={{display: 'block',
-        marginLeft: 'auto', marginRight: 'auto'}} />
+      <img src='spinner.gif' alt='loading...' className='loading' />
     );
   }
   return (
